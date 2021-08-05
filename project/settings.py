2e6d5a87bc43d8b072a9eed3df7f7b9afb22d8dc
Fatima <fatima@gmail.com>
2021-08-05 15:18:07 +0500

    other settings

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=c@yl4@kfwulww*4f7^v!h!^z+$khc6=ez*+o148i+rvx-31ej'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["what-todo-app-new.herokuapp.com/","127.0.0.1:8000/"]
 
 
 # Application definition
